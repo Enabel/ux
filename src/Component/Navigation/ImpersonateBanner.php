90f23b47d607d44a742269c3ea4dc8ae643b6530
Damien LAGAE <damienlagae@gmail.com>
2026-05-28 21:32:45 +0200

    ✨ Add ImpersonateBanner navigation component
    Closes #16.
    
    A fixed-top warning banner that signals the current user is
    impersonating someone, with a clear exit link. Ships the CSS trick
    that adjusts a Bootstrap fixed-top navbar to sit 28 px below the
    banner and bumps --app-navbar-offset to 108 px via body:has().
    
    Pairs with the upcoming Enabel:Ux:ImpersonateDropdown (#15).

---
+++ src/Component/Navigation/ImpersonateBanner.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Enabel UX package.
+ * Copyright (c) Enabel <https://enabel.be/>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Enabel\Ux\Component\Navigation;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+
+class ImpersonateBanner
+{
+    public string $message;
+    public string $exitUrl;
+    public string $exitLabel;
+    public string $icon;
+
+    /**
+     * @param array<string, mixed> $data
+     *
+     * @return array<string, mixed>
+     */
+    #[PreMount]
+    public function preMount(array $data): array
+    {
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+
+        return $resolver->resolve($data) + $data;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setIgnoreUndefined();
+        $resolver->setRequired(['message', 'exitUrl']);
+        $resolver->setDefaults([
+            'exitLabel' => 'Exit impersonation',
+            'icon' => 'fa6-solid:user-secret',
+        ]);
+
+        $resolver->setAllowedTypes('message', 'string');
+        $resolver->setAllowedTypes('exitUrl', 'string');
+        $resolver->setAllowedTypes('exitLabel', 'string');
+        $resolver->setAllowedTypes('icon', 'string');
+    }
+}
